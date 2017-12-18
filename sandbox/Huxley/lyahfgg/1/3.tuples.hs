e2f4b7dfad57e7b90fe7a27bd1c1dfc5edbe14d1
Unknown <jkpro550@gmail.com>
2017-12-18 21:57:09 +0700

    tuples added module init for runhaskell execution

--- sandbox/Huxley/lyahfgg/1/3.tuples.hs
+++ sandbox/Huxley/lyahfgg/1/3.tuples.hs
@@ -1,3 +1,5 @@
+module Main where
+
 tuple1 = (1, 2)
 
 list1 = [x * 2 | x <- [1 .. 20 ]]
@@ -6,4 +8,10 @@ list3 = zip list1 list2
 
 squad a = a * a
 isSquad a b c = squad a + squad b == squad c
-rightTriangles = [(a, b, c) | a <- [1 .. 10], b <- [1 .. 10], c <- [1 .. 10], (a + b + c ) == 24 && isSquad a b c]
+rightTriangles = [(a, b, c) | a <- [1 .. 10], 
+                              b <- [1 .. 10], 
+                              c <- [1 .. 10], 
+                              (a + b + c ) == 24 && isSquad a b c]
+
+
+main =  print (rightTriangles);
